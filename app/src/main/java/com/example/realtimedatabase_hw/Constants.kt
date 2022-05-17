8ea36406e5f658f7358bc344f9d6710bee458542
ismailhamad <ismailhamad2001@gmail.com>
2022-05-17 16:59:23 +0300

    book

--- app/src/main/java/com/example/realtimedatabase_hw/Constants.kt
+++ app/src/main/java/com/example/realtimedatabase_hw/Constants.kt
@@ -2,6 +2,6 @@ package com.example.realtimedatabase_hw
 
 object Constants {
     const val BASE_URL = "https://fcm.googleapis.com"
-    const val SERVER_KEY="AAAAAKzdFxo:APA91bFmoz-MXLmksMfx5nz-zg0aLR54O0nH45PSxEt-bK429Un9b8H1iMJcb9KLWd4cSJS_LnLeq0XrSZRkuE_Ip5wNzSgAdslj-_hXDxCfXZ61jzPQWjl-jvnfLqQgC_8Tm86-NjSw"
+    const val SERVER_KEY="AAAABLhfeK4:APA91bHNJxWyEo2njWrKwoEh6nOPXEr312Pzh4q3OqKp5nM-_QJ-A0qXK0r0QT6JIzAvk8jOlWvRxYVHYdVcA-YZwLK1v7lU7cfgLsmtW3S3LyEgX4aAb8TU4bliNLu4fTqVl3ozKOkl"
     const val CONTENT_TYPE = "application/json"
 }
